b61feeff592f3fed84578fdf08000b4c1c060ac7
Prince Kumar <140375727+sprince1204@users.noreply.github.com>
2024-11-13 13:39:05 +0530

    Update order data analysis.py

--- order data analysis.py
+++ order data analysis.py
@@ -1,7 +1,6 @@
 
 import kaggle
 
-!kaggle datasets download ankitbansal06/retail-orders -f orders.csv
 
 
 
@@ -21,43 +20,24 @@ zip_ref.close() # close file
 import pandas as pd
 df = pd.read_csv('orders.csv',na_values=['Not Available','unknown'])
 df['Ship Mode'].unique()
-
-
-
-
-#rename columns names ..make them lower case and replace space with underscore
-#df.rename(columns={'Order Id':'order_id', 'City':'city'})
-#df.columns=df.columns.str.lower()
-#df.columns=df.columns.str.replace(' ','_')
 df.head(5)
 
 
 
-
-#derive new columns discount , sale price and profit
-#df['discount']=df['list_price']*df['discount_percent']*.01
-#df['sale_price']= df['list_price']-df['discount']
 df['profit']=df['sale_price']-df['cost_price']
 df
 
 
-
-
-#convert order date from object data type to datetime
 df['order_date']=pd.to_datetime(df['order_date'],format="%Y-%m-%d")
 
 
 
-
-#drop cost price list price and discount percent columns
 df.drop(columns=['list_price','cost_price','discount_percent'],inplace=True)
 
 
 
-
-#load the data into sql server using replace option
 import sqlalchemy as sal
-engine = sal.create_engine('mssql://ANKIT\SQLEXPRESS/master?driver=ODBC+DRIVER+17+FOR+SQL+SERVER')
+engine = sal.create_engine('')
 conn=engine.connect()
 
 
